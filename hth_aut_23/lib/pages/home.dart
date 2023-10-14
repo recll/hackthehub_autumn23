6e4dc0a5e3e5bf1e2be0d8d7e4edd428aab404e2
Ryan R <>
2023-10-14 13:17:16 +0100

    Added some widgets to the home

--- hth_aut_23/lib/pages/home.dart
+++ hth_aut_23/lib/pages/home.dart
@@ -8,13 +8,39 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  double containerPadding = 5;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Mortgage Mentor")),
-      body: const Center(
-        child: Text("This is a placeholder"),
-      ),
-    );
+        appBar: AppBar(title: const Text("Mortgage Mentor")),
+        body: Row(
+          children: [
+            Container(
+              padding: EdgeInsets.all(containerPadding),
+              width: MediaQuery.of(context).size.width / 2,
+              color: const Color.fromARGB(255, 156, 220, 239),
+              child: Column(
+                children: [
+                  Text("This is the left")
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(containerPadding),
+              width: MediaQuery.of(context).size.width / 2,
+              child: Column(
+                children: [
+                  Text("This is the right"),
+                  ElevatedButton(
+                      onPressed: () {
+                        print("Pressed");
+                      },
+                      child: const Text("Click Me"))
+                ],
+              )
+            )
+          ],
+        ));
   }
 }
